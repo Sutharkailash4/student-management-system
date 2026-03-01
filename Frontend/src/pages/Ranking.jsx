28721e222ba00b481d8d59415df15c4b84d7281b
Kailash Sutahr <kailashsuthar4488@gmail.com>
2026-03-01 09:06:59 +0530

    rank-student

--- Frontend/src/pages/Ranking.jsx
+++ Frontend/src/pages/Ranking.jsx
@@ -1,46 +1,60 @@
-import React, { useContext} from "react";
+import React, { useContext } from "react";
 import { Student_data_context } from "../studentContext/SudentContext";
 
 const Ranking = () => {
+  const { data } = useContext(Student_data_context);
 
-    const {data,setData} = useContext(Student_data_context);
+  const studentsWithTotal = data.map((student) => {
+    const total = student.marks.reduce((sum, m) => sum + m.score, 0);
+    return { ...student, total };
+  });
 
-    return (
+  const sortedStudents = [...studentsWithTotal].sort(
+    (a, b) => b.total - a.total
+  );
+
+  return (
     <div className="ranking_main_box">
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Total</th>
-                    <th>Percentage</th>
-                    <th>Grade</th>
-                    <th>Rank</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((elem,idx)=>{
-                    const Total = elem.marks.reduce((sum,num)=>{
-                        return sum+num.score;
-                    },0)
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Total</th>
+            <th>Percentage</th>
+            <th>Grade</th>
+            <th>Rank</th>
+          </tr>
+        </thead>
 
-                    const Percentage = Total/500*100;
+        <tbody>
+          {sortedStudents.map((elem, idx) => {
+            const Percentage = (elem.total / 500) * 100;
 
-                    const Grade = Percentage>90 && Percentage<101 ? "A" : Percentage>85 && Percentage<91 ? "B" : Percentage>75 && Percentage<85 ? "C" : Percentage>40 && Percentage<75 ? "D" : "Fail" 
+            const Grade =
+              Percentage >= 90
+                ? "A"
+                : Percentage >= 85
+                ? "B"
+                : Percentage >= 75
+                ? "C"
+                : Percentage >= 40
+                ? "D"
+                : "Fail";
 
-                    return (
-                        <tr key={idx}>
-                            <td className="td">{elem.name}</td>
-                            <td className="td">{Total}</td>
-                            <td className="td">{Percentage}%</td>
-                            <td className="td">{Grade}</td>
-                            <td className="td">Rank</td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+            return (
+              <tr key={idx}>
+                <td>{elem.name}</td>
+                <td>{elem.total}</td>
+                <td>{Percentage.toFixed(2)}%</td>
+                <td>{Grade}</td>
+                <td>{idx + 1}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-    )
-}
+  );
+};
 
-export default Ranking; 
+export default Ranking;
